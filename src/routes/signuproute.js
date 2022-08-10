c76ed2155125603a309c80ed1c3ed4599c27cb09
Rajeev C <81346240+rajeevc5260@users.noreply.github.com>
2022-08-10 20:29:32 +0530

    update

--- src/routes/signuproute.js
+++ src/routes/signuproute.js
@@ -1,23 +1,20 @@
-const express = require('express'); 
+const express = require("express");
 const signupRouter = express.Router();
-const user = require('../data/user');
+const user = require("../data/user");
 
-signupRouter.get('/',function(req,res){
+signupRouter.get("/", function (req, res) {
+  res.render("signup", {});
+});
 
-    res.render('signup',{});
-    
-})
-
-signupRouter.get("/adduser",function(req,res){
-    
-    var newuser = {
-        "uid":req.param("uid"),
-        "pwd":req.param("pwd")
-    };
-    console.log(newuser);
-    user.push(newuser);
-    console.log(user);
-    res.redirect("/login");
-})
+signupRouter.get("/adduser", function (req, res) {
+  var newuser = {
+    uid: req.param("uid"),
+    pwd: req.param("pwd"),
+  };
+  console.log(newuser);
+  user.push(newuser);
+  console.log(user);
+  res.redirect("/login");
+});
 
 module.exports = signupRouter;
