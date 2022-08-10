158f1b9ae5219f07e4108d7ab5c7135bca13af89
Rajeev C <81346240+rajeevc5260@users.noreply.github.com>
2022-08-10 11:29:12 +0530

    updates

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library');
+const mongoose = require("mongoose");
+// mongoose.connect('mongodb://localhost:27017/Library');
+
+const url = "mongodb://localhost:27017/Library";
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const Schema = mongoose.Schema;
 
-
 const AuthorSchema = new Schema({
-    title : String,
-    image: String,
-    about: String
+  title: String,
+  image: String,
+  about: String,
 });
 
-const authordata = mongoose.model('authordata',AuthorSchema);
+const authordata = mongoose.model("authordata", AuthorSchema);
 
 module.exports = authordata;
--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library');
+const mongoose = require("mongoose");
+// mongoose.connect('mongodb://localhost:27017/Library');
+const url = "mongodb://localhost:27017/Library";
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const Schema = mongoose.Schema;
 
-
 const BookSchema = new Schema({
-    title : String,
-    author: String,
-    image: String,
-    about: String
+  title: String,
+  author: String,
+  image: String,
+  about: String,
 });
 
-const bookdata = mongoose.model('bookdata',BookSchema);
+const bookdata = mongoose.model("bookdata", BookSchema);
 
 module.exports = bookdata;
